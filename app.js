077e685afc3c816d5afa66244376b1e1a8eef222
aiaishwa <aishwarya.dhiman@nokia.com>
2021-09-11 15:08:50 +0530

    api integrated

---
+++ app.js
@@ -0,0 +1,35 @@
+const clickBtn = document.querySelector("#click-btn");
+const textInput = document.querySelector("#text-content");
+const outputDiv = document.querySelector("#output-content")
+
+
+
+
+
+// const serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+
+const serverUrl = "https://api.funtranslations.com/translate/minion.json"
+
+const newURL = function (text) {
+    return `${serverUrl}?text="${text}"`;
+
+}
+
+
+const doFetch = function () {
+    let inputText = textInput.value;
+    console.log(inputText);
+    fetch(newURL(inputText)).then(response => response.json()).then(json => outputDiv.innerText = json.contents.translated).catch(error => {
+        console.log("There is an error", error)
+        alert("Sorry ! The server is down 😢 Please try after some time !")
+    })
+
+
+}
+
+clickBtn.addEventListener("click", doFetch
+    //outputDiv.innerText = "hello there ! You clicked a button";
+    // outputDiv.innerHTML = "hiiiii";
+
+)
