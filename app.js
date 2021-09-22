09529b24b8f574e0c9d5081e0809a73fd5adf992
aiaishwa <aishwarya.dhiman@nokia.com>
2021-09-22 23:56:02 +0530

    added style

--- app.js
+++ app.js
@@ -2,14 +2,12 @@ const clickBtn = document.querySelector("#click-btn");
 const textInput = document.querySelector("#text-content");
 const outputDiv = document.querySelector("#output-content")
 
-
-
-
-
 // const serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
 
 
 const serverUrl = "https://api.funtranslations.com/translate/minion.json"
+
+
 
 const newURL = function (text) {
     return `${serverUrl}?text="${text}"`;
@@ -19,17 +17,19 @@ const newURL = function (text) {
 
 const doFetch = function () {
     let inputText = textInput.value;
-    console.log(inputText);
-    fetch(newURL(inputText)).then(response => response.json()).then(json => outputDiv.innerText = json.contents.translated).catch(error => {
-        console.log("There is an error", error)
-        alert("Sorry ! The server is down 😢 Please try after some time !")
-    })
+
+    if (inputText == "") {
+        console.log("no text entered by user")
+        alert("Please Enter Text");
+    } else {
+        console.log(inputText);
+        fetch(newURL(inputText)).then(response => response.json()).then(json => outputDiv.innerText = json.contents.translated).catch(error => {
+            console.log("There is an error", error)
+            alert("Sorry ! The server is down 😢 Please try after some time !")
+        })
 
 
+    }
 }
 
-clickBtn.addEventListener("click", doFetch
-    //outputDiv.innerText = "hello there ! You clicked a button";
-    // outputDiv.innerHTML = "hiiiii";
-
-)
+clickBtn.addEventListener("click", doFetch);
